335306c8b58b138970f954b74f621bfbc6a01450
Maxim Abdulkhalikov <63835313+maxakadem1@users.noreply.github.com>
2023-09-27 05:32:14 -0500

    Contact page done

--- src/components/ContactMeContent.tsx
+++ src/components/ContactMeContent.tsx
@@ -1,4 +1,7 @@
 import { animated } from '@react-spring/web'
+import Link from 'next/link'
+// icons
+import { FiArrowUpRight } from 'react-icons/fi'
 
 interface Props {
   resumeProps: any
@@ -8,14 +11,46 @@ interface Props {
 const ContactMeContent: React.FC<Props> = ({ resumeProps, goBack }) => {
   return (
     <animated.div style={resumeProps}>
-      <div className='flex flex-col gap-2 justify-start'>
-        My Contact Content Here
-        <button
-          onClick={goBack}
-          className='text-gray-500 hover:text-white w-fit'
-        >
-          Go Back
-        </button>
+      <div className='flex flex-col h-[75vh] '>
+        <div className='pb-6'>
+          <div className='text-xl font-semibold text-gray-300'>Contacts</div>
+          <button
+            onClick={goBack}
+            className='text-gray-500 hover:text-white w-fit'
+          >
+            Go Back
+          </button>
+        </div>
+
+        <div className='flex flex-col gap-2 overflow-y-auto'>
+          <Link
+            href='https://www.linkedin.com/in/abdulkhalikov/'
+            target='_blank'
+            rel='noopener noreferrer'
+            className='flex flex-row items-center justify-center text-gray-500 hover:text-white w-fit'
+          >
+            <h1 className=''>Email</h1>
+            <FiArrowUpRight className='ml-1' size={17} />
+          </Link>
+          <Link
+            href='https://www.linkedin.com/in/abdulkhalikov/'
+            target='_blank'
+            rel='noopener noreferrer'
+            className='flex flex-row items-center justify-center text-gray-500 hover:text-white w-fit'
+          >
+            <h1 className=''>LinkedIn</h1>
+            <FiArrowUpRight className='ml-1' size={17} />
+          </Link>
+          <Link
+            href='https://github.com/maxakadem1'
+            target='_blank'
+            rel='noopener noreferrer'
+            className='flex flex-row items-center justify-center text-gray-500 hover:text-white w-fit'
+          >
+            <h1 className=''>GitHub</h1>
+            <FiArrowUpRight className='ml-1' size={17} />
+          </Link>
+        </div>
       </div>
     </animated.div>
   )
