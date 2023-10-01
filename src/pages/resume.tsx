f8b848d00d435159134bc90ac3c407711ad10a3f
Maxim Abdulkhalikov <63835313+maxakadem1@users.noreply.github.com>
2023-10-01 08:43:49 -0500

    resume added

--- src/pages/resume.tsx
+++ src/pages/resume.tsx
@@ -1,4 +1,4 @@
-//libraries
+// libraries
 import Head from 'next/head'
 import '../app/globals.css'
 
@@ -9,8 +9,24 @@ export default function ResumePage() {
         <title>Resume</title>
       </Head>
 
-      <main className='flex h-screen w-scre en flex-row items-center justify-between gap-12 p-10 text-sm overflow-hidden bg-black'>
-        <div className='h-[100vh] w-[100vw] bg-black'>text</div>
+      <main className='flex h-screen w-screen flex-row items-center justify-between gap-12 p-10 text-sm overflow-hidden bg-black'>
+        <div className='h-[100vh] w-[100vw] bg-black'>
+          <iframe
+            className='h-full w-full'
+            src='/Resume.pdf'
+            onError={(e) => {
+              e.currentTarget.style.display = 'none'
+              const fallbackMessage = document.getElementById('fallbackMessage')
+              if (fallbackMessage) {
+                fallbackMessage.style.display = 'block'
+              }
+            }}
+          ></iframe>
+          <div id='fallbackMessage' style={{ display: 'none' }}>
+            Your browser does not support PDFs.
+            <a href='/Resume.pdf'>Download PDF</a>
+          </div>
+        </div>
       </main>
     </div>
   )
